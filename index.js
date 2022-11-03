addca705c30f16dbca27007a75d640c24883c49f
Prithibee13 <66945275+Prithibee13@users.noreply.github.com>
2022-11-04 00:17:58 +0600

    restaurent

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 
@@ -78,6 +78,17 @@ async function run() {
       let restaurants;
       restaurants = await cursor.toArray();
       res.send(restaurants)
+    })
+
+    app.get("/restaurants/:id" , async (req , res) =>
+    {
+      const id = req.params.id
+      const query = {_id : ObjectId(id)}
+      const cursor =  restaurantsCollection.find(query);
+      let restaurant;
+      restaurant = await cursor.toArray();
+      res.send(restaurant)
+
     })
 
     app.post("/addCollection" , async (req,res)=>{
