412ee83ad42984c65aa24f710158f3d041204c25
Prithibee13 <66945275+Prithibee13@users.noreply.github.com>
2022-11-06 22:19:25 +0600

    bug

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ async function run() {
     })
 
 
-    app.get("foods/:id" , async(req,res)=>
+    app.get("/foods/:id" , async(req,res)=>
     {
         const id = req.params.id;
         const query = { restraurentId : id }
